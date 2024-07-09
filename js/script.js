36b5656b8c0109b20aa628463015ec8a6be74dc7
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-09 15:12:05 +0200

    [UPD] defined the condition it the number generated is already inside the array

--- js/script.js
+++ js/script.js
@@ -6,5 +6,9 @@ function generateRandomNum (){
     while (randomNum.length < 5){
         // TRAMITE I COMANTI MATH DEFINISCO LA CREAZIONE DI UN NUMERO ENTRO UN DETERMINATO RANGE DI NUMERI (IN QUESTO CASO DA 1 A 100)
         let num = Math.floor(Math.random() * 100 + 1);
+        // DEFINISCO LE CONDIZIONI PER VERIFICARE CHE I NUMERI NON SI RIPETANO ALL'INTERNO DELL'ARRAY
+        if (!randomNum.incluedes(num)){
+            
+        }
     }
 }
